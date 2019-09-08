2b318dddf8a7489d7b0dcda11bebc61c45f2c3fe
Apurva khadkikar <apurvakhadkikar2699@gmail.com>
2019-09-08 14:22:42 +0300

    Create Arithmatic.R

---
+++ Arithmatic.R
@@ -0,0 +1,13 @@
+a <-3
+b <-4
+c=a+b
+print(c)
+d<-c*10
+print(c("multiplication is:",d))
+print(c("addition is:",c))
+e<-d/2
+print(c("division is:",e))
+f<- e%%2
+print(c("mode arithmatic is:",f))
+
+
